eee3a458764783d53e165690c3c4502c682f5c6c
SergeyLepeshinsky <thsname18@gmail.com>
2021-08-05 21:39:50 +0300

    Создана модель Пользователя

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя пола. </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+            Name = name;
+        }
+        /// <summary>
+        /// Переопределение метода Тустринг в классе Gender.
+        /// </summary>
+        /// <returns>Имя пола. </returns>
+        public override string ToString()  
+        {
+            return Name;
+        }
+    }
+}
